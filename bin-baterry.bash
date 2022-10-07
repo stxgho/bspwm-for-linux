4f695cb031bedf2aeceea9865a67b7a9aec119ba
stxgho <110248582+srghostx@users.noreply.github.com>
2022-10-07 16:54:51 -0300

    Create bin-baterry.bash

---
+++ bin-baterry.bash
@@ -0,0 +1,11 @@
+# bin-baterry
+script for baterry
+
+#!/bin/bash
+
+if [ "$(acpi | awk '{print $3}' | tr -d ',')" != "Discharging" ]; then
+
+        echo -e "%{F#27FF00} %{F#e2ee6a}$(acpi awk | awk  '{print $4}' | tr -d ',')%{u-}"
+else
+        echo -e "%{F#FF0000} %{F#e2ee6a}$(acpi awk | awk '{print $4}' | tr -d ',')%{u-}"
+fi
